f1dc9a07250b259eb897e8c7d45fd8b8d66cedd0
Stephanie Villalobos <svillalobos2027@gmail.com>
2026-04-29 12:17:15 -0700

    Initial Commit

---
+++ SummerOlympics/SummerOlympicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SummerOlympicsApp.swift
+//  SummerOlympics
+//
+//  Created by VILLALOBOS, STEPHANIE on 4/29/26.
+//
+
+import SwiftUI
+
+@main
+struct SummerOlympicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
